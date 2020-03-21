2d3ee6b45b42f77b7371e6ccd8ec0a944b4b8e1e
aaronshivers <aaron@moonmail.org>
2020-03-21 11:20:44 -0500

    add hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
